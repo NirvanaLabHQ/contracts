0c6a464de3ed69a8be7475e27484b51476d4b907
yuanjunliang <1610712512@qq.com>
2023-03-10 00:56:32 +0800

    add chainlinkVRF proxy

---
+++ src/nft/ChainLinkVRFProxy.sol
@@ -0,0 +1,98 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity 0.8.17;
+
+import "@chainlink/contracts/src/v0.8/interfaces/VRFCoordinatorV2Interface.sol";
+import "@chainlink/contracts/src/v0.8/VRFConsumerBaseV2.sol";
+import "@chainlink/contracts/src/v0.8/ConfirmedOwner.sol";
+
+contract ChainLinkVRF is VRFConsumerBaseV2, ConfirmedOwner {
+    struct RequestStatus {
+        bool fulfilled;
+        bool exists;
+        uint256[] randomWords;
+    }
+
+    event RequestRandomWords(uint256 requestId);
+    event FulfilledRandomWords(uint256 requestId, uint256[] randomWords);
+
+    address owner;
+    uint64 subscriptionId;
+    uint16 requestConfirmations;
+    VRFCoordinatorV2Interface public coordinator;
+    bytes32 public keyHash;
+    uint256 public latestRequestId;
+    uint256[] public requestIds;
+    mapping(uint256 => RequestStatus) public requests;
+
+    constructor(
+        address owner_, // control contract
+        address consumer_,
+        address coordinator_,
+        bytes32 keyHash_
+    ) VRFConsumerBaseV2(consumer_) ConfirmedOwner(owner_) {
+        require(owner != address(0), "Ownable: owner is the zero address");
+        owner = owner;
+    }
+
+    function requestRandomWords(
+        uint32 numWords,
+        uint32 callbackGasLimit
+    ) external onlyOwner returns (uint256 requestId) {
+        requestId = coordinator.requestRandomWords(
+            keyHash,
+            subscriptionId,
+            requestConfirmations,
+            callbackGasLimit,
+            numWords
+        );
+        requests[requestId] = RequestStatus({
+            fulfilled: false,
+            exists: true,
+            randomWords: new uint256[](0)
+        });
+        requestIds.push(requestId);
+        latestRequestId = requestId;
+
+        emit RequestRandomWords(requestId);
+    }
+
+    /**
+     * @notice owner must implament
+     * fulfillRandomWordsCallback(uint256 _requestId,uint256[] memory randomWords) function to receive randomWords
+     * @dev receive randomWords from chainlink and notify owner
+     * @param requestId requestId of randomwords
+     * @param randomWords random words of requestId return from chainlink vrf
+     */
+    function fulfillRandomWords(
+        uint256 requestId,
+        uint256[] memory randomWords
+    ) internal override {
+        require(requests[requestId].exists, "request not found");
+
+        requests[requestId].fulfilled = true;
+        requests[requestId].randomWords = randomWords;
+
+        owner.call{value: 23000}(
+            abi.encodeWithSignature(
+                "fulfillRandomWordsCallback(uint256 _requestId,uint256[] memory randomWords)",
+                requestId,
+                randomWords
+            )
+        );
+
+        emit FulfilledRandomWords(requestId, randomWords);
+    }
+
+    function getRequestIds() external view returns (uint256[] memory) {
+        return requestIds;
+    }
+
+    function getReuqestStatus(
+        uint256 requestId
+    ) external view returns (bool fulfilled, uint256[] memory randomWords) {
+        require(requests[requestId].exists, "requestId not exists");
+
+        RequestStatus memory status = requests[requestId];
+        return (status.fulfilled, status.randomWords);
+    }
+}
